f19b54e4e9cfc3531849c4a024c2c423e64b62bb
noel-anthony-w <code@noelweiss.de>
2026-07-27 19:59:16 +0200

    feat: added profile view/edit and added security features from episode 3

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Pages\EditProfile;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -27,9 +28,18 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->unsavedChangesAlerts()
+            ->databaseTransactions()
+            ->registration()
+            ->passwordReset()
+            ->profile(EditProfile::class)
+            ->emailVerification()
+            ->emailChangeVerification()
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::convertToOklch('#4397cb'),
+                'gray' => Color::Slate,
+                'info' => Color::Violet,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
